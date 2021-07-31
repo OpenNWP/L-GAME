e556beca0f2969011a202f50f65ee3f5670b8903
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2021-08-01 00:13:39 +0200

    control.f modified

--- src/control.f
+++ src/control.f
@@ -1,15 +1,28 @@
 ! This source file is part of the Regional Forecasting with Poisson-brackets in Exner-Theta formulation (RFPET), which is released under the MIT license.
 ! Github repository: https://github.com/OpenNWP/RFPET
 
-use grad
-
 program control
 
-  ! firstly, the grid generator needs to be called to calculate the grid properties
-  call grid_generator()
-  
-  
-  ! the loop over the time steps
-  while
+	! This controls the model run from the beginning to the end.
+
+	! reading in all namelists so that we know what we have to do
+	call read_all_namelists()
+
+	! firstly, the grid generator needs to be called to calculate the grid properties
+	call grid_generator()
+
+	! reading the initial state
+	call read_init()
+
+	! the loop over the time steps
+	cont_steppint = .true.
+	do while (cont_steppint)
+		
+		
+		
+	enddo
   
 end program control
+
+
+
