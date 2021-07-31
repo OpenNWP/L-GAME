169c2cd6cdf1795a53b622363171c444e7470e04
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2021-08-01 00:52:25 +0200

    some files added

--- run_scripts/ideal.sh
+++ run_scripts/ideal.sh
@@ -3,6 +3,7 @@
 # This source file is part of the Regional Forecasting with Poisson-brackets in Exner-Theta formulation (RFPET), which is released under the MIT license.
 # Github repository: https://github.com/OpenNWP/RFPET
 
+rfpet_home_dir=~/code/rfpet
 run_id=ideal
 
 # That's it, here we go.
--- run_scripts/root_script.sh
+++ run_scripts/root_script.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-if [ ! -d output/$run_id ]
+# the directory of this run
+run_dir=$rfpet_home_dir/output/$run_id
+
+# creating the run directory if it does not exist
+if [ ! -d $run_dir ]
 then
-mkdir output/$run_id
+mkdir $run_id
 fi
+
+cd $run_dir
+
+cp ../bin/rfpet .
+./rfpet
+
+
