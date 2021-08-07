20aa94dd7e6076ff8aa3e4b97ad18fff9cc7f5bd
Max H. Balsmeier <mhbalsmeier@gmail.com>
2021-08-07 23:17:40 +0200

    bugfix in vertical solver

--- src/time_stepping/vertical_slice_solvers.f90
+++ src/time_stepping/vertical_slice_solvers.f90
@@ -101,15 +101,15 @@ module vertical_slice_solvers
 				! density, potential temperature density
 				do jl=2,nlays-1
 					state_new%rho(ji+1,jk+1,jl) = rho_expl(jl) + dtime*(-solution(jl-1)+solution(jl))
-					state_new%rhotheta(ji+1,jk+1,jl) = rho_theta_expl(jl) + &
+					state_new%rhotheta(ji+1,jk+1,jl) = rhotheta_expl(jl) + &
 					dtime*(-theta_int_expl(jl-1)*solution(jl-1)+theta_int_expl(jl)*solution(jl))
 				enddo
 				! uppermost layer
 				state_new%rho(ji+1,jk+1,1) = rho_expl(1) + dtime*solution(1)
-				state_new%rhotheta(ji+1,jk+1,1) = rho_theta_expl(1) + dtime*theta_int_expl(1)*solution(1)
+				state_new%rhotheta(ji+1,jk+1,1) = rhotheta_expl(1) + dtime*theta_int_expl(1)*solution(1)
 				! lowest layer
 				state_new%rho(ji+1,jk+1,nlays) = rho_expl(nlays) - dtime*solution(nlays-1)
-				state_new%rhotheta(ji+1,jk+1,nlays) = rho_theta_expl(nlays) - dtime*theta_int_expl(nlays-1)*solution(nlays-1)
+				state_new%rhotheta(ji+1,jk+1,nlays) = rhotheta_expl(nlays) - dtime*theta_int_expl(nlays-1)*solution(nlays-1)
 				! vertical velocity
 				do jl=2,nlays
 					rho_int_new = 0.5_wp*(state_new%rho(ji+1,jk+1,jl-1)+state_new%rho(ji+1,jk+1,jl))
