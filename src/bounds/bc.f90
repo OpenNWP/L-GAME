b5c4e7474a8c7c1e8deafb0ebb92b2e7cbb7479f
Max H. Balsmeier <mhbalsmeier@gmail.com>
2021-08-10 12:33:58 +0200

    minor

---
+++ src/bounds/bc.f90
@@ -0,0 +1,30 @@
+! This source file is part of the Regional Forecasting with Poisson-brackets in Exner-Theta formulation (RFPET), which is released under the MIT license.
+! Github repository: https://github.com/OpenNWP/RFPET
+
+module bc
+
+  ! This module handles everything dealing with boundary conditions.
+
+  implicit none
+  
+  private
+  
+  public :: boundaries
+  
+  contains
+  
+  subroutine boundaries()
+  
+    ! sets the boundary conditions
+    
+  end subroutine boundaries
+
+end module bc
+
+
+
+
+
+
+
+
