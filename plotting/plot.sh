ed045c4ae204d3d649586982d7359fc60448c25e
Max H. Balsmeier <mhbalsmeier@gmail.com>
2021-08-16 12:14:26 +0200

    plot scripts prepared

--- plotting/plot.sh
+++ plotting/plot.sh
@@ -2,18 +2,3 @@
 
 # This source file is part of the Limited-area GAME version (L-GAME), which is released under the MIT license.
 # Github repository: https://github.com/OpenNWP/L-GAME
-
-if [ ! -d output ]
-then
-mkdir output
-fi
-
-if [ ! -d bc ]
-then
-mkdir bc
-fi
-
-if [ ! -d figs ]
-then
-mkdir figs
-fi
