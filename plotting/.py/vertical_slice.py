246e542a055bb91b63efdf889448347ecc679ad7
Max H. Balsmeier <mhbalsmeier@gmail.com>
2021-08-16 13:09:50 +0200

    plot routines continued

--- plotting/.py/vertical_slice.py
+++ plotting/.py/vertical_slice.py
@@ -1,7 +1,8 @@
-#!/bin/bash
-
 # This source file is part of the Limited-area GAME version (L-GAME), which is released under the MIT license.
 # Github repository: https://github.com/OpenNWP/L-GAME
 
-run_id=ideal
-plot_time_since_init_min=2
+import sys;
+import matplotlib.pyplot as plt;
+import numpy as np;
+
+
