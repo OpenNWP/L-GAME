0229f87fc52660c436932d49fb034a6024166870
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-02-20 21:12:39 +0100

    effective diffusion coefficients file updated

--- src/subgrid_scale/effective_diff_coeffs.f90
+++ src/subgrid_scale/effective_diff_coeffs.f90
@@ -11,7 +11,52 @@ module effective_diff_coeffs
   
   private
   
+  public :: hori_div_viscosity
+  public :: hori_curl_viscosity
+  public :: vert_hori_mom_viscosity
+  public :: vert_vert_mom_viscosity
+  public :: temp_diffusion_coeffs
+  public :: mass_diffusion_coeffs
+  
   contains
+  
+  subroutine hori_div_viscosity()
+  
+    ! This function computes the effective diffusion coefficient (molecular + turbulent) acting on horizontal divergent movements.
+  
+  end subroutine hori_div_viscosity
+  
+  subroutine hori_curl_viscosity()
+  
+    ! This function computes the effective diffusion coefficient (molecular + turbulent) acting on horizontal curl movements.
+  
+  end subroutine hori_curl_viscosity
+  
+  subroutine vert_hori_mom_viscosity()
+  
+    ! This function computes the effective viscosity (Eddy + molecular viscosity) for the vertical diffusion of horizontal velocity.
+	! This quantity is located at the half level edges.
+	! To obey the symmetry of the stress tensor, the same coefficient must be used for the horizontal diffusion of vertical velocity.
+  
+  end subroutine vert_hori_mom_viscosity
+  
+  subroutine vert_vert_mom_viscosity()
+  
+    ! This function multiplies scalar_field_placeholder (containing dw/dz) by the diffusion coefficient acting on w because of w.
+  
+  end subroutine vert_vert_mom_viscosity
+  
+  subroutine temp_diffusion_coeffs()
+  
+    ! This function computes the viscous temperature diffusion coefficient (including eddies).
+  
+  end subroutine temp_diffusion_coeffs
+  
+  subroutine mass_diffusion_coeffs()
+  
+    ! This function computes the viscous tracer diffusion coefficient (including eddies).
+  
+  end subroutine mass_diffusion_coeffs
   
   function tke2vertical_diff_coeff(tke)
     
@@ -32,3 +77,10 @@ module effective_diff_coeffs
   end function tke2vertical_diff_coeff
   
 end module effective_diff_coeffs
+
+
+
+
+
+
+
