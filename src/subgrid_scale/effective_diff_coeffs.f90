fedffb1bd78debf1b07e912be67af9d979a2bd2f
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-02-20 20:57:24 +0100

    turbulence stuff continued

--- src/subgrid_scale/effective_diff_coeffs.f90
+++ src/subgrid_scale/effective_diff_coeffs.f90
@@ -5,4 +5,30 @@ module effective_diff_coeffs
   
   ! This module computes the effective diffusion coefficients.
   
+  use definitions, only: wp
+  
+  implicit none
+  
+  private
+  
+  contains
+  
+  function tke2vertical_diff_coeff(tke)
+    
+    ! This function returns the vertical kinematic Eddy viscosity as a function of the specific TKE.
+	
+    ! input
+    real(wp), intent(in) :: tke
+	
+    ! output
+    real(wp)             :: tke2vertical_diff_coeff
+    
+    ! local variable
+    real(wp)             :: prop_constant
+	
+    prop_constant = 0.4_wp ! unit: m
+    tke2vertical_diff_coeff = prop_constant*tke**0.5_wp
+	
+  end function tke2vertical_diff_coeff
+  
 end module effective_diff_coeffs
