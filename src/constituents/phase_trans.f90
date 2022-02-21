36845d2c7f6ecd5c59fb860a6a3bf412879007d3
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-02-21 22:55:46 +0100

    phase transition rates calculation continued

--- src/constituents/phase_trans.f90
+++ src/constituents/phase_trans.f90
@@ -5,7 +5,36 @@ module phase_trans
 
   ! In this module, phase transition rates are being calculated.
   
-	
+  use run_nml, only: nlins,ncols,nlays
+  
+  implicit none
+  
+  private
+  
+  public :: calc_h2otracers_source_rates
+  
+  contains
+  
+  subroutine calc_h2otracers_source_rates()
+  
+    ! This subroutine calculates the phase transition rates.
+  
+    ! local variables
+    integer :: ji,jk,jl
+  
+    !$OMP PARALLEL
+    !$OMP DO PRIVATE(ji,jk,jl)
+    do ji=1,nlays
+      do jk=1,ncols
+        do jl=1,nlays
+          
+        enddo
+      enddo
+    enddo
+    !$OMP END DO
+    !$OMP END PARALLEL
+  
+  end subroutine calc_h2otracers_source_rates
 
 end module phase_trans
 
