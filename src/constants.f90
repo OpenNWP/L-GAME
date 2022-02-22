52667bb9e4997b0f13fe1748cd3473b5d8460a0a
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-02-22 22:02:40 +0100

    formal improvements

--- src/constants.f90
+++ src/constants.f90
@@ -14,12 +14,15 @@ module constants
   real(wp) :: re = 6371000.789927_wp      ! Earth radius
   real(wp) :: k_B = 1.380649e-23_wp       ! Boltzmann's constant
   real(wp) :: T_0 = 273.15_wp             ! 273.15 K
-  real(wp) :: EPSILON_SECURITY = 1e-10_wp ! security constant
   real(wp) :: density_water = 1024._wp    ! typical density of water
-  real(wp) :: M_PI = 4._wp*atan(1._wp)    ! pi
   real(wp) :: p_0 = 100000._wp            ! reference pressure
   real(wp) :: omega = 7.292115e-5_wp      ! angular frequency of Earth rotation
   real(wp) :: gravity = 9.80616_wp        ! average surface gravity value
+  
+  ! non-physical constants
+  ! ----------------------
+  real(wp) :: M_PI = 4._wp*atan(1._wp)    ! pi
+  real(wp) :: EPSILON_SECURITY = 1e-10_wp ! security constant
   
   ! some properties of the standard atmosphere
   ! ------------------------------------------
