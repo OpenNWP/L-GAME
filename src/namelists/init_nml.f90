6ee700b1dcafea076c6975d6941009727228e255
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-02-23 18:48:08 +0100

    init namelist removed

--- src/namelists/init_nml.f90
+++
@@ -1,6 +0,0 @@
-! This source file is part of the Limited-area GAME version (L-GAME), which is released under the MIT license.
-! Github repository: https://github.com/OpenNWP/L-GAME
-
-module init_nml
-	
-end module init_nml
