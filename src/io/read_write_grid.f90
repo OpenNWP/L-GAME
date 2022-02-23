9af98dc803ed292296547d0bc3a0f498b7f8aa0c
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-02-24 00:23:30 +0100

    structural improvements

--- src/io/read_write_grid.f90
+++ src/io/read_write_grid.f90
@@ -5,7 +5,26 @@ module set_grid
 
   ! This module reads the grid from a file or writes the grid to a file. This is useful for efficiency.
 
-   
+  implicit none
+  
+  private
+  
+  public :: write_grid
+  public :: read_grid
+  
+  contains
+  
+  subroutine write_grid
+  
+    ! This subroutine writes important grid properties to a file.
+  
+  end subroutine write_grid
+  
+  subroutine read_grid
+  
+    ! This subroutine reads important grid properties from a file.
+  
+  end subroutine read_grid
 
 end module set_grid
 
