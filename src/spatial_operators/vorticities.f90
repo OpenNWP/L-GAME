8c869f38d6593bc78036f3fac5c27975c1b6c4aa
Max H. Balsmeier <mhbalsmeier@gmail.com>
2022-02-26 22:07:08 +0100

    bugfixes

--- src/spatial_operators/vorticities.f90
+++ src/spatial_operators/vorticities.f90
@@ -37,17 +37,17 @@ module vorticities
     ! calculating the relative vorticity in x-direction
     !$OMP PARALLEL
     !$OMP DO PRIVATE(ji,jk,jl)
-    do ji=1,nlins+1
+    do ji=2,nlins
       do jk=1,ncols
         do jl=2,nlays
           diag%z_eta_x(ji,jk,jl) = &
-          + grid%dz(ji+1,jk+1,jl)*state%wind_w(ji+1,jk+1,jl)                                         &
-          - grid%dy(ji,jk+1,jl-1)*horizontal_covariant_y(state%wind_v,state%wind_w,grid,ji,jk+1,jl-1)  &
-          - grid%dz(ji,jk+1,jl)*state%wind_w(ji,jk+1,jl)                                         &
-          + grid%dy(ji,jk+1,jl)*horizontal_covariant_y(state%wind_v,state%wind_w,grid,ji,jk+1,jl)
+          + grid%dz(ji,jk,jl)*state%wind_w(ji,jk,jl) &
+          - grid%dy(ji,jk,jl-1)*horizontal_covariant_y(state%wind_v,state%wind_w,grid,ji,jk,jl-1) &
+          - grid%dz(ji-1,jk,jl)*state%wind_w(ji-1,jk,jl) &
+          + grid%dy(ji,jk,jl)*horizontal_covariant_y(state%wind_v,state%wind_w,grid,ji,jk,jl)
         enddo
         ! At the surface, w vanishes. Furthermore, the covariant velocity below the surface is also zero.
-        diag%z_eta_x(ji,jk,nlays+1) = -grid%dy(ji,jk+1,nlays)*horizontal_covariant_y(state%wind_v,state%wind_w,grid,ji,jk+1,nlays)
+        diag%z_eta_x(ji,jk,nlays+1) = -grid%dy(ji,jk,nlays)*horizontal_covariant_y(state%wind_v,state%wind_w,grid,ji,jk,nlays)
       enddo
     enddo
     !$OMP END DO
@@ -61,16 +61,16 @@ module vorticities
     !$OMP PARALLEL
     !$OMP DO PRIVATE(ji,jk,jl)
     do ji=1,nlins
-      do jk=1,ncols+1
+      do jk=2,ncols
         do jl=2,nlays
           diag%z_eta_y(ji,jk,jl) = &
-          + grid%dz(ji+1,jk,jl)*state%wind_w(ji+1,jk,jl)                                              &
-          + grid%dx(ji+1,jk,jl-1)*horizontal_covariant_x(state%wind_u,state%wind_w,grid,ji+1,jk,jl-1) &
-          - grid%dz(ji+1,jk+1,jl)*state%wind_w(ji+1,jk+1,jl)                                          &
-          - grid%dx(ji+1,jk,jl)*horizontal_covariant_x(state%wind_u,state%wind_w,grid,ji+1,jk,jl)
+          - grid%dz(ji,jk,jl)*state%wind_w(ji,jk,jl) &
+          - grid%dx(ji,jk,jl)*horizontal_covariant_x(state%wind_u,state%wind_w,grid,ji,jk,jl) &
+          + grid%dz(ji,jk-1,jl)*state%wind_w(ji,jk-1,jl) &
+          + grid%dx(ji,jk,jl-1)*horizontal_covariant_x(state%wind_u,state%wind_w,grid,ji,jk,jl-1)
         enddo
         ! At the surface, w vanishes. Furthermore, the covariant velocity below the surface is also zero.
-        diag%z_eta_y(ji,jk,nlays+1) = grid%dx(ji+1,jk,nlays)*horizontal_covariant_x(state%wind_u,state%wind_w,grid,ji+1,jk,nlays)
+        diag%z_eta_y(ji,jk,nlays+1) = grid%dx(ji,jk,nlays)*horizontal_covariant_x(state%wind_u,state%wind_w,grid,ji,jk,nlays)
       enddo
     enddo
     !$OMP END DO
@@ -96,48 +96,48 @@ module vorticities
         ! layers which follow the orography
         do jl=nlays-nlays_oro+1,nlays
           ! first
-          l_rescale = (re + grid%z_geo_area_dual_z(ji,jk,jl))/(re + grid%z_geo_v(ji,jk+1,jl))
-          delta_z = grid%z_geo_area_dual_z(ji,jk,jl) - grid%z_geo_v(ji,jk+1,jl)
-          ind_shift = 1
-          if (delta_z>0._wp .or. jl==nlays) then
-            ind_shift = -1
-          endif
-          vertical_gradient = (state%wind_v(ji,jk,jl) - state%wind_v(ji,jk,jl+ind_shift))/ &
-          (grid%z_geo_v(ji,jk+1,jl) - grid%z_geo_v(ji,jk+1,jl+ind_shift))
-          diag%z_eta_z(ji,jk,jl) = l_rescale*grid%dy(ji,jk,jl)* &
-          (state%wind_v(ji,jk,jl) + delta_z*vertical_gradient)
-          ! second
-          l_rescale = (re + grid%z_geo_area_dual_z(ji,jk,jl))/(re + grid%z_geo_u(ji+1,jk,jl))
-          delta_z = grid%z_geo_area_dual_z(ji,jk,jl) - grid%z_geo_u(ji+1,jk,jl)
-          ind_shift = 1
-          if (delta_z>0._wp .or. jl==nlays) then
-            ind_shift = -1
-          endif
-          vertical_gradient = (state%wind_u(ji,jk,jl) - state%wind_u(ji,jk,jl+ind_shift))/ &
-          (grid%z_geo_u(ji+1,jk,jl) - grid%z_geo_u(ji+1,jk,jl+ind_shift))
-          diag%z_eta_z(ji,jk,jl) = diag%z_eta_z(ji,jk,jl) - l_rescale*grid%dx(ji,jk,jl)* &
-          (state%wind_u(ji,jk,jl) + delta_z*vertical_gradient)
-          ! third
           l_rescale = (re + grid%z_geo_area_dual_z(ji,jk,jl))/(re + grid%z_geo_v(ji,jk,jl))
           delta_z = grid%z_geo_area_dual_z(ji,jk,jl) - grid%z_geo_v(ji,jk,jl)
           ind_shift = 1
           if (delta_z>0._wp .or. jl==nlays) then
             ind_shift = -1
           endif
-          vertical_gradient = (state%wind_v(ji,jk-1,jl) - state%wind_v(ji,jk-1,jl+ind_shift))/ &
+          vertical_gradient = (state%wind_v(ji,jk,jl) - state%wind_v(ji,jk,jl+ind_shift))/ &
           (grid%z_geo_v(ji,jk,jl) - grid%z_geo_v(ji,jk,jl+ind_shift))
-          diag%z_eta_z(ji,jk,jl) = diag%z_eta_z(ji,jk,jl) - l_rescale*grid%dy(ji,jk-1,jl)* &
-          (state%wind_v(ji,jk-1,jl) + delta_z*vertical_gradient)
-          ! fourth
+          diag%z_eta_z(ji,jk,jl) = l_rescale*grid%dy(ji,jk,jl)* &
+          (state%wind_v(ji,jk,jl) + delta_z*vertical_gradient)
+          ! second
           l_rescale = (re + grid%z_geo_area_dual_z(ji,jk,jl))/(re + grid%z_geo_u(ji,jk,jl))
           delta_z = grid%z_geo_area_dual_z(ji,jk,jl) - grid%z_geo_u(ji,jk,jl)
           ind_shift = 1
           if (delta_z>0._wp .or. jl==nlays) then
             ind_shift = -1
           endif
-          vertical_gradient = (state%wind_u(ji-1,jk,jl) - state%wind_u(ji-1,jk,jl+ind_shift))/ &
+          vertical_gradient = (state%wind_u(ji,jk,jl) - state%wind_u(ji,jk,jl+ind_shift))/ &
           (grid%z_geo_u(ji,jk,jl) - grid%z_geo_u(ji,jk,jl+ind_shift))
-          diag%z_eta_z(ji,jk,jl) = diag%z_eta_z(ji,jk,jl) + l_rescale*grid%dx(ji-1,jk,jl)* &
+          diag%z_eta_z(ji,jk,jl) = diag%z_eta_z(ji,jk,jl) - l_rescale*grid%dx(ji,jk,jl)* &
+          (state%wind_u(ji,jk,jl) + delta_z*vertical_gradient)
+          ! third
+          l_rescale = (re + grid%z_geo_area_dual_z(ji,jk-1,jl))/(re + grid%z_geo_v(ji,jk-1,jl))
+          delta_z = grid%z_geo_area_dual_z(ji,jk-1,jl) - grid%z_geo_v(ji,jk-1,jl)
+          ind_shift = 1
+          if (delta_z>0._wp .or. jl==nlays) then
+            ind_shift = -1
+          endif
+          vertical_gradient = (state%wind_v(ji,jk-1,jl) - state%wind_v(ji,jk-1,jl+ind_shift))/ &
+          (grid%z_geo_v(ji,jk-1,jl) - grid%z_geo_v(ji,jk-1,jl+ind_shift))
+          diag%z_eta_z(ji,jk-1,jl) = diag%z_eta_z(ji,jk-1,jl) - l_rescale*grid%dy(ji,jk-1,jl)* &
+          (state%wind_v(ji,jk-1,jl) + delta_z*vertical_gradient)
+          ! fourth
+          l_rescale = (re + grid%z_geo_area_dual_z(ji-1,jk,jl))/(re + grid%z_geo_u(ji-1,jk,jl))
+          delta_z = grid%z_geo_area_dual_z(ji-1,jk,jl) - grid%z_geo_u(ji-1,jk,jl)
+          ind_shift = 1
+          if (delta_z>0._wp .or. jl==nlays) then
+            ind_shift = -1
+          endif
+          vertical_gradient = (state%wind_u(ji-1,jk,jl) - state%wind_u(ji-1,jk,jl+ind_shift))/ &
+          (grid%z_geo_u(ji-1,jk,jl) - grid%z_geo_u(ji-1,jk,jl+ind_shift))
+          diag%z_eta_z(ji-1,jk,jl) = diag%z_eta_z(ji-1,jk,jl) + l_rescale*grid%dx(ji-1,jk,jl)* &
           (state%wind_u(ji-1,jk,jl) + delta_z*vertical_gradient)
         enddo
       enddo
