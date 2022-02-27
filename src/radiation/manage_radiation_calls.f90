6bf3aa28264d993ae7a3c6db27aa01ee05fb811c
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-02-27 23:09:07 +0100

    formal changes

--- src/radiation/manage_radiation_calls.f90
+++ src/radiation/manage_radiation_calls.f90
@@ -19,10 +19,10 @@ module manage_radiation_calls
   
     ! calls RTE+RRTMGP
     
-    type(t_state),  intent(inout) :: state_old          ! the state at the old timestep
-    type(t_grid),   intent(inout) :: grid               ! the grid of the model
-    type(t_diag),   intent(inout) :: diag               ! diagnostic quantities
-    type(t_irrev),  intent(inout) :: irrev              ! irreversible quantities
+    type(t_state),  intent(inout) :: state_old ! the state at the old timestep
+    type(t_grid),   intent(inout) :: grid      ! the grid of the model
+    type(t_diag),   intent(inout) :: diag      ! diagnostic quantities
+    type(t_irrev),  intent(inout) :: irrev     ! irreversible quantities
     
     write(*,*) "Starting update of radiative fluxes ..."
     
