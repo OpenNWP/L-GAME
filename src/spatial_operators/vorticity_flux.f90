66df559e071b70c05b81c66d58d73ccda4d4f795
Max H. Balsmeier <mhbalsmeier@gmail.com>
2022-02-28 15:02:16 +0100

    bugfix

--- src/spatial_operators/vorticity_flux.f90
+++ src/spatial_operators/vorticity_flux.f90
@@ -74,13 +74,13 @@ module vorticity_flux
     !$OMP PARALLEL
     !$OMP DO PRIVATE(ji,jk,jl)
     do ji=1,nlins
-      do jk=1,ncols-1
+      do jk=2,ncols
         do jl=1,nlays
           diag%pot_vort_tend_x(ji,jk,jl) = diag%pot_vort_tend_x(ji,jk,jl) &
-          - 0.5_wp*grid%inner_product_weights(ji,jk,jl,5)*diag%w_placeholder(ji,jk,jl)*diag%eta_y(ji,jk+1,jl) &
-          - 0.5_wp*grid%inner_product_weights(ji,jk,jl,6)*diag%w_placeholder(ji,jk,jl+1)*diag%eta_y(ji,jk+1,jl+1) &
-          - 0.5_wp*grid%inner_product_weights(ji,jk+1,jl,5)*diag%w_placeholder(ji,jk+1,jl)*diag%eta_y(ji,jk+1,jl) &
-          - 0.5_wp*grid%inner_product_weights(ji,jk+1,jl,6)*diag%w_placeholder(ji,jk+1,jl+1)*diag%eta_y(ji,jk+1,jl+1)
+          - 0.5_wp*grid%inner_product_weights(ji,jk-1,jl,5)*diag%w_placeholder(ji,jk-1,jl)*diag%eta_y(ji,jk,jl) &
+          - 0.5_wp*grid%inner_product_weights(ji,jk-1,jl,6)*diag%w_placeholder(ji,jk-1,jl+1)*diag%eta_y(ji,jk,jl+1) &
+          - 0.5_wp*grid%inner_product_weights(ji,jk,jl,5)*diag%w_placeholder(ji,jk,jl)*diag%eta_y(ji,jk,jl) &
+          - 0.5_wp*grid%inner_product_weights(ji,jk,jl,6)*diag%w_placeholder(ji,jk,jl+1)*diag%eta_y(ji,jk,jl+1)
         enddo
       enddo
     enddo
@@ -89,14 +89,14 @@ module vorticity_flux
     ! v
     !$OMP PARALLEL
     !$OMP DO PRIVATE(ji,jk,jl)
-    do ji=1,nlins-1
+    do ji=2,nlins
       do jk=1,ncols
         do jl=1,nlays
           diag%pot_vort_tend_y(ji,jk,jl) = diag%pot_vort_tend_y(ji,jk,jl) &
-          + 0.5_wp*grid%inner_product_weights(ji,jk,jl,5)*diag%w_placeholder(ji,jk,jl)*diag%eta_x(ji+1,jk,jl) &
-          + 0.5_wp*grid%inner_product_weights(ji,jk,jl,6)*diag%w_placeholder(ji,jk,jl+1)*diag%eta_x(ji+1,jk,jl+1) &
-          + 0.5_wp*grid%inner_product_weights(ji+1,jk,jl,5)*diag%w_placeholder(ji+1,jk,jl)*diag%eta_x(ji+1,jk,jl) &
-          + 0.5_wp*grid%inner_product_weights(ji+1,jk,jl,6)*diag%w_placeholder(ji+1,jk,jl+1)*diag%eta_x(ji+1,jk,jl+1)
+          + 0.5_wp*grid%inner_product_weights(ji-1,jk,jl,5)*diag%w_placeholder(ji-1,jk,jl)*diag%eta_x(ji,jk,jl) &
+          + 0.5_wp*grid%inner_product_weights(ji-1,jk,jl,6)*diag%w_placeholder(ji-1,jk,jl+1)*diag%eta_x(ji,jk,jl+1) &
+          + 0.5_wp*grid%inner_product_weights(ji,jk,jl,5)*diag%w_placeholder(ji,jk,jl)*diag%eta_x(ji,jk,jl) &
+          + 0.5_wp*grid%inner_product_weights(ji,jk,jl,6)*diag%w_placeholder(ji,jk,jl+1)*diag%eta_x(ji,jk,jl+1)
         enddo
       enddo
     enddo
@@ -115,7 +115,7 @@ module vorticity_flux
           + grid%inner_product_weights(ji,jk,jl-1,3)*diag%u_placeholder(ji,jk,jl-1)*diag%eta_y(ji,jk,jl) &
           - grid%inner_product_weights(ji,jk,jl-1,4)*diag%v_placeholder(ji+1,jk,jl-1)*diag%eta_x(ji+1,jk,jl) &
           + grid%inner_product_weights(ji,jk,jl,1)*diag%u_placeholder(ji,jk+1,jl)*diag%eta_y(ji,jk+1,jl) &
-          - grid%inner_product_weights(ji,jk,jl,2)*diag%v_placeholder(ji,jk,jl)*diag%eta_x(ji+1,jk,jl) &
+          - grid%inner_product_weights(ji,jk,jl,2)*diag%v_placeholder(ji,jk,jl)*diag%eta_x(ji,jk,jl) &
           + grid%inner_product_weights(ji,jk,jl,3)*diag%u_placeholder(ji,jk,jl)*diag%eta_y(ji,jk,jl) &
           - grid%inner_product_weights(ji,jk,jl,4)*diag%v_placeholder(ji+1,jk,jl)*diag%eta_x(ji+1,jk,jl))
         enddo
