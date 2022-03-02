cef7f981585b94b724788a5f393ebe215bb41bce
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-03-02 22:51:53 +0100

    diffusion continued

--- src/subgrid_scale/effective_diff_coeffs.f90
+++ src/subgrid_scale/effective_diff_coeffs.f90
@@ -5,12 +5,14 @@ module effective_diff_coeffs
   
   ! This module computes the effective diffusion coefficients.
   
-  use run_nml,            only: nlins,ncols,nlays
-  use definitions,        only: wp,t_state,t_diag,t_irrev,t_grid
-  use diff_nml,           only: diff_h_smag_div
-  use derived_quantities, only: calc_diffusion_coeff
-  use constituents_nml,   only: no_of_condensed_constituents
-  use tke,                only: tke_update
+  use run_nml,              only: nlins,ncols,nlays
+  use definitions,          only: wp,t_state,t_diag,t_irrev,t_grid
+  use diff_nml,             only: diff_h_smag_div,lmom_diff_h,ltemp_diff_h
+  use derived_quantities,   only: calc_diffusion_coeff
+  use constituents_nml,     only: no_of_condensed_constituents
+  use tke,                  only: tke_update
+  use divergence_operators, only: divv_h
+  use derived_quantities,   only: density_gas
   
   implicit none
   
@@ -64,9 +66,15 @@ module effective_diff_coeffs
   
   end subroutine hori_div_viscosity
   
-  subroutine hori_curl_viscosity()
+  subroutine hori_curl_viscosity(state,diag,irrev,grid)
   
     ! This subroutine computes the effective diffusion coefficient (molecular + turbulent) acting on horizontal curl movements.
+  
+    ! input arguments and output
+    type(t_state), intent(in)    :: state ! state
+    type(t_diag),  intent(inout) :: diag  ! diagnostic quantities
+    type(t_irrev), intent(inout) :: irrev ! irreversible quantities
+    type(t_grid),  intent(in)    :: grid  ! grid quantities
   
   end subroutine hori_curl_viscosity
   
@@ -99,10 +107,67 @@ module effective_diff_coeffs
   
   end subroutine temp_diffusion_coeffs
   
-  subroutine mass_diffusion_coeffs()
+  subroutine mass_diffusion_coeffs(state,diag,irrev,grid)
   
     ! This subroutine computes the viscous tracer diffusion coefficient (including eddies).
   
+    ! input arguments and output
+    type(t_state), intent(in)    :: state ! state
+    type(t_diag),  intent(inout) :: diag  ! diagnostic quantities
+    type(t_irrev), intent(inout) :: irrev ! irreversible quantities
+    type(t_grid),  intent(in)    :: grid  ! grid quantities
+    
+    ! local variables
+    integer :: ji,jk,jl ! looop variables
+    
+    ! The eddy viscosity coefficient and the TKE only has to be calculated if it has not yet been done.
+    if (lmom_diff_h .and. .not. ltemp_diff_h) then
+    
+      call divv_h(state%wind_u,state%wind_v,diag%scalar_placeholder,grid)
+      call hori_div_viscosity(state,diag,diag%scalar_placeholder,irrev,grid)
+      call hori_curl_viscosity(state,diag,irrev,grid)
+      call tke_update(state,diag,irrev,grid)
+      
+      ! molecular viscosity
+      !$OMP PARALLEL
+      !$OMP DO PRIVATE(ji,jk,jl)
+      do ji=1,nlins
+        do jk=1,ncols
+          do jl=1,nlays
+            irrev%viscosity_molecular(ji,jk,jl) = calc_diffusion_coeff(diag%temperature_gas(ji,jk,jl), &
+            state%rho(ji,jk,jl,no_of_condensed_constituents+1))
+          enddo
+        enddo
+      enddo
+      !$OMP END DO
+      !$OMP END PARALLEL
+    
+    endif
+    
+    !$OMP PARALLEL
+    !$OMP DO PRIVATE(ji,jk,jl)
+    do ji=1,nlins
+      do jk=1,ncols
+        do jl=1,nlays
+          ! horizontal diffusion coefficient
+          irrev%scalar_diff_coeff_h(ji,jk,jl) &
+          ! molecular component
+          = irrev%viscosity_molecular(ji,jk,jl) &
+          ! turbulent component
+          + (irrev%viscosity_coeff_div(ji,jk,jl) + irrev%viscosity_coeff_curl(ji,jk,jl)) &
+          /density_gas(state,ji,jk,jl)
+          ! vertical diffusion coefficient
+          irrev%scalar_diff_coeff_v(ji,jk,jl) &
+          ! molecular component
+          = irrev%viscosity_molecular(ji,jk,jl) &
+          ! turbulent component
+          + tke2vertical_diff_coeff(irrev%tke(ji,jk,jl))
+        enddo
+      enddo
+    enddo
+    !$OMP END DO
+    !$OMP END PARALLEL
+    
   end subroutine mass_diffusion_coeffs
   
   function tke2vertical_diff_coeff(tke)
