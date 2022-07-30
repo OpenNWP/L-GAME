b793c3d54181ca6a853410eaf99fa318606d6138
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-07-31 00:44:31 +0200

    formal changes

--- src/constants.f90
+++ src/constants.f90
@@ -13,28 +13,28 @@ module constants
   
   ! physical constants
   ! ------------------
-  real(wp) :: r_e = 6371000.789927_wp ! Earth radius
-  real(wp) :: k_b = 1.380649e-23_wp   ! Boltzmann's constant
-  real(wp) :: n_a =  6.02214076e23_wp ! Avogadro's number
-  real(wp) :: t_0 = 273.15_wp         ! 273.15 K
-  real(wp) :: rho_h2o = 1024._wp      ! typical density of water
-  real(wp) :: p_0 = 100000._wp        ! reference pressure
-  real(wp) :: omega = 7.292115e-5_wp  ! angular frequency of Earth rotation
-  real(wp) :: gravity = 9.80616_wp    ! average surface gravity value
+  real(wp), parameter :: r_e = 6371000.789927_wp ! Earth radius
+  real(wp), parameter :: k_b = 1.380649e-23_wp   ! Boltzmann's constant
+  real(wp), parameter :: n_a =  6.02214076e23_wp ! Avogadro's number
+  real(wp), parameter :: t_0 = 273.15_wp         ! 273.15 K
+  real(wp), parameter :: rho_h2o = 1024._wp      ! typical density of water
+  real(wp), parameter :: p_0 = 100000._wp        ! reference pressure
+  real(wp), parameter :: omega = 7.292115e-5_wp  ! angular frequency of Earth rotation
+  real(wp), parameter :: gravity = 9.80616_wp    ! average surface gravity value
   
   ! non-physical constants
   ! ----------------------
-  real(wp) :: M_PI = 4._wp*atan(1._wp)    ! pi
-  real(wp) :: EPSILON_SECURITY = 1e-10_wp ! security constant
+  real(wp), parameter :: M_PI = 4._wp*atan(1._wp)    ! pi
+  real(wp), parameter :: EPSILON_SECURITY = 1e-10_wp ! security constant
   
   ! some properties of the standard atmosphere
   ! ------------------------------------------
-  real(wp) :: lapse_rate = 0.0065_wp    ! lapse_rate within the troposphere
-  real(wp) :: surface_temp = 288.15_wp  ! the temperature at the surface
-  real(wp) :: tropo_height = 11000._wp  ! the tropopause height
-  real(wp) :: inv_height = 20000._wp    ! height where the temperature inversion begins
-  real(wp) :: t_grad_inv = 0.001_wp     ! temperature gradient above the inversion
-  real(wp) :: p_0_standard = 101325._wp ! reference pressure of the standard atmosphere
+  real(wp), parameter :: lapse_rate = 0.0065_wp    ! lapse_rate within the troposphere
+  real(wp), parameter :: surface_temp = 288.15_wp  ! the temperature at the surface
+  real(wp), parameter :: tropo_height = 11000._wp  ! the tropopause height
+  real(wp), parameter :: inv_height = 20000._wp    ! height where the temperature inversion begins
+  real(wp), parameter :: t_grad_inv = 0.001_wp     ! temperature gradient above the inversion
+  real(wp), parameter :: p_0_standard = 101325._wp ! reference pressure of the standard atmosphere
 
 end module constants
 
