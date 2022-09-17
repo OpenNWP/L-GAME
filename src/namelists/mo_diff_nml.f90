c9afa51149b7c61723836e5cbbe87034b94869ea
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-09-17 13:18:23 +0200

    formal changes

--- src/namelists/mo_diff_nml.f90
+++ src/namelists/mo_diff_nml.f90
@@ -9,22 +9,20 @@ module mo_diff_nml
   
   implicit none
   
-  logical  :: lklemp          ! turns the Klemp damping layer on or off
-  real(wp) :: klemp_damp_max  ! the maximum Klemp damping coefficient
-  real(wp) :: klemp_begin_rel ! lower boundary of the Klemp damping layer in relation to the TOA
   logical  :: lmom_diff_h     ! switch for horizontal momentum diffusion
   logical  :: lmom_diff_v     ! switch for vertical momentum diffusion
-  real(wp) :: diff_h_smag_div ! horizontal diffusion Smagorinsky factor acting on divergent movements
-  real(wp) :: diff_h_smag_rot ! horizontal diffusion Smagorinsky factor acting on vortical movements
   logical  :: ltemp_diff_h    ! horizontal temperature diffusion switch
   logical  :: ltemp_diff_v    ! vertical temperature diffusion switch
   logical  :: lmass_diff_h    ! horizontal mass diffusion switch
   logical  :: lmass_diff_v    ! vertical mass diffusion switch
   real(wp) :: h_prandtl       ! height of the Prandtl layer
   real(wp) :: karman          ! von Karman's constant
+  logical  :: lklemp          ! turns the Klemp damping layer on or off
+  real(wp) :: klemp_damp_max  ! the maximum Klemp damping coefficient
+  real(wp) :: klemp_begin_rel ! lower boundary of the Klemp damping layer in relation to the TOA
   
-  namelist /diff/lklemp,klemp_damp_max,klemp_begin_rel,lmom_diff_h,lmom_diff_v,diff_h_smag_div,diff_h_smag_rot, &
-  ltemp_diff_h,ltemp_diff_v,lmass_diff_h,lmass_diff_v,karman,lklemp,klemp_damp_max,klemp_begin_rel
+  namelist /diff/lmom_diff_h,lmom_diff_v,ltemp_diff_h,ltemp_diff_v,lmass_diff_h,lmass_diff_v,h_prandtl,karman, &
+                 lklemp,klemp_damp_max,klemp_begin_rel
   
   contains
   
@@ -34,25 +32,54 @@ module mo_diff_nml
     integer :: fileunit
     
     ! default values
-    lklemp = .true.
-    klemp_damp_max = 0.25_wp
-    klemp_begin_rel = 0.53_wp
+    h_prandtl = 100._wp
     lmom_diff_h = .true.
     lmom_diff_v = .true.
-    diff_h_smag_div = 0.01_wp
-    diff_h_smag_rot = 0.01_wp
     ltemp_diff_h = .true.
     ltemp_diff_v = .true.
     lmass_diff_h = .true.
     lmass_diff_v = .true.
-    h_prandtl = 100._wp
     karman = 0.4_wp
+    lklemp = .true.
+    klemp_damp_max = 0.25_wp
+    klemp_begin_rel = 0.53_wp
     
     ! Open and read namelist file.
     open(action="read",file="namelist.nml",newunit=fileunit)
     read(nml=diff,unit=fileunit)
         
     close(fileunit)
+    
+    if (lmom_diff_h) then
+      write(*,*) "Horizontal momentum diffusion is turned on."
+    else
+      write(*,*) "Horizontal momentum diffusion is turned off."
+    endif
+    if (lmom_diff_v) then
+      write(*,*) "Vertical momentum diffusion is turned on."
+    else
+      write(*,*) "Vertical momentum diffusion is turned off."
+    endif
+    if (ltemp_diff_h) then
+      write(*,*) "Horizontal temperature diffusion is turned on."
+    else
+      write(*,*) "Horizontal temperature diffusion is turned off."
+    endif
+    if (ltemp_diff_v) then
+      write(*,*) "Vertical temperature diffusion is turned on."
+    else
+      write(*,*) "Vertical temperature diffusion is turned off."
+    endif
+    if (lmass_diff_h) then
+      write(*,*) "Horizontal mass diffusion is turned on."
+    else
+      write(*,*) "Horizontal mass diffusion is turned off."
+    endif
+    if (lmass_diff_v) then
+      write(*,*) "Vertical mass diffusion is turned on."
+    else
+      write(*,*) "Vertical mass diffusion is turned off."
+    endif
   
   end subroutine diff_nml_setup
   
