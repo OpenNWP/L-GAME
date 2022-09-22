b208d3e44334c0ef1803f4c5c47fe6dd6604ada9
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-09-22 18:18:35 +0200

    cloud physics from GAME synchronized

--- src/constituents/mo_dictionary.f90
+++ src/constituents/mo_dictionary.f90
@@ -3,21 +3,7 @@
 
 module mo_dictionary
 
-  ! In this module, properties of the constituents are being stored.
-  
-  ! gaseous constituents IDs:
-  ! 0: dry air
-  ! 1: H2O
-  ! 2: N2
-  ! 3: O2
-  ! 4: Ar
-  ! 5: CO2
-  ! 6: Ne
-  ! 7: He
-  ! 8: CH4
-  ! 9: CO
-  ! 10: O3
-  ! 11: N2O
+  ! This module contains look-up functions for properties of the atmosphere.
 
   use mo_definitions, only: wp
   use mo_constants,   only: t_0,n_a,r_v,m_v
@@ -25,10 +11,24 @@ module mo_dictionary
   implicit none
   
   contains
-
+  
   function molar_fraction_in_dry_air(gas_number)
     
     ! This function returns the molar fraction of certain gases in dry air.
+    
+    ! gaseous constituents IDs:
+    ! 0: dry air
+    ! 1: H2O
+    ! 2: N2
+    ! 3: O2
+    ! 4: Ar
+    ! 5: CO2
+    ! 6: Ne
+    ! 7: He
+    ! 8: CH4
+    ! 9: CO
+    ! 10: O3
+    ! 11: N2O
     
     integer, intent(in) :: gas_number
     real(wp)            :: molar_fraction_in_dry_air
@@ -61,7 +61,7 @@ module mo_dictionary
       molar_fraction_in_dry_air = 1e-6_wp
     endif
     if (gas_number==11) then
-	  ! https://www.epa.gov/climate-indicators/climate-change-indicators-atmospheric-concentrations-greenhouse-gases
+    ! https://www.epa.gov/climate-indicators/climate-change-indicators-atmospheric-concentrations-greenhouse-gases
       molar_fraction_in_dry_air = 0.3e-6_wp
     endif
   
@@ -76,26 +76,26 @@ module mo_dictionary
     real(wp)             :: calc_o3_vmr
     
     ! local variables
-    real(wp) :: fwhm = 20e3_wp      ! full width at half maximum
-    real(wp) :: max = 34e3_wp       ! height of the maximum of the distribution
-    real(wp) :: max_vmr = 8.5e-6_wp ! maximum volume mixing ratio
-    real(wp) :: sigma               ! standard deviation
-    real(wp) :: distance            ! distance from the maximum
+    real(wp) :: fwhm = 20e3_wp       ! full width at half maximum
+    real(wp) :: max_height = 34e3_wp ! height of the maximum of the distribution
+    real(wp) :: max_vmr = 8.5e-6_wp  ! maximum volume mixing ratio
+    real(wp) :: sigma                ! standard deviation
+    real(wp) :: distance             ! distance from the maximum
     
     ! calculation of the result
     sigma = fwhm/(8._wp*log(2._wp))**0.5_wp
-    distance = height - max
+    distance = height-max_height
     calc_o3_vmr = max_vmr*exp(-distance**2/(2._wp*sigma**2))
     
   end function calc_o3_vmr
-  
+
   function phase_trans_heat(direction,temperature)
     
     ! This function calculates the phase transition heat.
 
     ! input arguments
-    integer, intent(in)  :: direction
-    real(wp), intent(in) :: temperature
+    integer  :: direction
+    real(wp) :: temperature
 
     real(wp) :: phase_trans_heat
     
@@ -117,12 +117,91 @@ module mo_dictionary
     
   end function phase_trans_heat
   
+  function c_p_water(temperature)
+
+    ! This function returns c_p of water.
+  
+    real(wp), intent(in) :: temperature
+    real(wp)             :: c_p_water
+  
+    ! local variables
+    real(wp) :: temp_c
+  
+    ! calculating the temperature in degrees Celsius
+    temp_c = temperature - t_0
+  
+    ! For "positive" temperatures we use the formula cited in Pruppacher and Klett (2010), p. 93, Eq. (3-15).
+  
+    if (temp_c>=0._wp) then
+      ! clipping values that are too extreme for this approximation
+      if (temp_c>35._wp) then
+        temp_c = 35._wp
+      endif
+      c_p_water = 0.9979_wp + 3.1e-6_wp*(temp_c-35._wp)**2 + 3.8e-9_wp*(temp_c-35._wp)**4
+    ! This is the case of supercooled water. We use the formula cited in Pruppacher and Klett (2010), p. 93, Eq. (3-16).
+    else
+      ! clipping values that are too extreme for this approximation
+      if (temp_c<-37._wp) then
+        temp_c = -37._wp
+      endif
+      c_p_water = 1.000938_wp - 2.7052e-3_wp*temp_c - 2.3235e-5_wp*temp_c**2 + 4.3778e-6_wp*temp_c**3 + 2.7136e-7_wp*temp_c**4
+    endif
+    
+    ! unit conversion from IT cal/(g*K) to J/(kg*K)
+    c_p_water = 4186.8_wp*c_p_water
+  
+  end function c_p_water
+  
+  function c_p_ice(temperature)
+  
+    ! This function returns c_p of ice.
+    ! It follows Eq. (4) in Murphy DM, Koop T. Review of the vapour pressures of ice and supercooled water for atmospheric applications.
+    ! QUARTERLY JOURNAL OF THE ROYAL METEOROLOGICAL SOCIETY. 2005;131(608):1539-1565.
+  
+    real(wp), intent(in) :: temperature
+    real(wp)             :: c_p_ice
+    
+    ! local variables
+    real(wp) :: temperature_local
+    
+    temperature_local = temperature
+  
+    ! ice cannot exist in equilibrium at temperatures > T_0
+    if (temperature_local>t_0) then
+      temperature_local = t_0
+    endif
+    ! clipping values that are too extreme for this approximation
+    if (temperature_local<20._wp) then
+      temperature_local = 20._wp
+    endif
+    c_p_ice = -2.0572_wp + 0.14644_wp*temperature_local + 0.06163_wp*temperature_local*exp(-(temperature_local/125.1_wp)**2)
+    ! unit conversion from J/(mol*K) to J/(kg*K)
+    c_p_ice = c_p_ice/m_v
+    
+  end function c_p_ice
+
+  function c_p_cond(const_id,temperature)
+  
+    ! This function resturns c_p of a specific condensed constituent.
+    
+    integer, intent(in)  :: const_id
+    real(wp), intent(in) :: temperature
+    real(wp)             :: c_p_cond
+  
+    if (mod(const_id-1,2)==0) then
+      c_p_cond = c_p_ice(temperature)
+    else
+      c_p_cond = c_p_water(temperature)
+    endif
+  
+  end function c_p_cond
+  
   function enthalpy_evaporation(temperature)
     
     ! This function returns the enthalpy of evaporation depending on the temperature.
     
     real(wp), intent(in) :: temperature
-    real(wp) :: enthalpy_evaporation
+    real(wp)             :: enthalpy_evaporation
     
     ! local variables
     real(wp) :: temperature_local
@@ -153,9 +232,9 @@ module mo_dictionary
   
   function enthalpy_sublimation(temperature)
     
-	! This function returns the enthalpy of sublimation depending on the temperature.
-	! It follows Eq. (5) in Murphy DM, Koop T. Review of the vapour pressures of ice and supercooled water for atmospheric applications.
-	! QUARTERLY JOURNAL OF THE ROYAL METEOROLOGICAL SOCIETY. 2005;131(608):1539-1565.
+  ! This function returns the enthalpy of sublimation depending on the temperature.
+  ! It follows Eq. (5) in Murphy DM, Koop T. Review of the vapour pressures of ice and supercooled water for atmospheric applications.
+  ! QUARTERLY JOURNAL OF THE ROYAL METEOROLOGICAL SOCIETY. 2005;131(608):1539-1565.
     
     real(wp), intent(in) :: temperature
     real(wp)             :: enthalpy_sublimation
@@ -164,7 +243,7 @@ module mo_dictionary
     real(wp) :: temperature_local
     
     temperature_local = temperature
-
+     
     ! clipping values that are too extreme for this approximation
     if (temperature_local<30._wp) then
       temperature_local = 30._wp
@@ -181,7 +260,7 @@ module mo_dictionary
     enthalpy_sublimation = enthalpy_sublimation/m_v
   
   end function enthalpy_sublimation
-	
+  
   function saturation_pressure_over_water(temperature)
 
     ! This function returns the saturation pressure in Pa over liquid water as a function of the temperature in K.
@@ -218,6 +297,33 @@ module mo_dictionary
     endif
 
   end function saturation_pressure_over_water
+  
+  function dsaturation_pressure_over_water_dT(temperature)
+  
+    ! This function returns the derivative of the saturation pressure in Pa of pure water vapour over plane liquid water
+    ! as a function of the temperature in K.
+    
+    real(wp), intent(in) :: temperature
+    real(wp)             :: dsaturation_pressure_over_water_dT
+    
+    ! local variables
+    real(wp) :: temp_c
+    
+    ! calculating the temperature in degrees Celsius
+    temp_c = temperature - t_0
+    
+    ! these are the limits of this approximation
+    if (temp_c>100._wp) then
+      temp_c = 100._wp
+    endif
+    if (temp_c<0._wp) then
+      temp_c = 0._wp
+    endif
+    
+    dsaturation_pressure_over_water_dT = saturation_pressure_over_water(temperature) &
+    *(4924.99_wp/(temp_c + 237.1_wp)**2 - 1.57_wp/(temp_c + 105._wp))
+  
+  end function dsaturation_pressure_over_water_dT
 
   function saturation_pressure_over_ice(temperature)
     
@@ -244,14 +350,40 @@ module mo_dictionary
     
   end function saturation_pressure_over_ice
   
+  function dsaturation_pressure_over_ice_dT(temperature)
+
+    ! This function returns derivative of the the saturation pressure in Pa of pure water vapour over plane ice
+    ! as a function of the temperature in K.
+    
+    real(wp), intent(in) :: temperature
+    real(wp)             :: dsaturation_pressure_over_ice_dT
+    
+    ! local variables
+    real(wp) :: temp_c
+    
+    ! calculating the temperature in degrees Celsius
+    temp_c = temperature - t_0
+    
+    ! this is the stability limit
+    if (temp_c<-80._wp) then
+      temp_c = -80._wp
+    endif
+    ! at temperatures > 0 degrees Celsius ice cannot exist in equilibrium which is why this is clipped
+    if (temp_c>0._wp) then
+      temp_c = 0._wp
+    endif
+    
+    dsaturation_pressure_over_ice_dT = saturation_pressure_over_ice(temperature) &
+    *(6545.8_wp/(temp_c + 278._wp)**2 - 2._wp/(temp_c + 868._wp))
+
+  end function dsaturation_pressure_over_ice_dT
+  
   function enhancement_factor_over_water(air_pressure)
 
     ! This function calculates the enhancement factor over water, which accounts for the fact the the saturation vapour pressure is different in moist air compared to pure water vapour.
     ! It uses the formula by Huang: A Simple Accurate Formula for Calculating Saturation Vapor Pressure of Water and Ice, 2018, DOI: 10.1175/JAMC-D-17-0334.1.
 
-    ! input arguments
     real(wp), intent(in) :: air_pressure
-    ! output argument
     real(wp)             :: enhancement_factor_over_water
     
     enhancement_factor_over_water = 0.99882_wp*exp(0.00000008_wp*air_pressure)
@@ -268,7 +400,7 @@ module mo_dictionary
     
     enhancement_factor_over_ice = 0.99882_wp*exp(0.00000008_wp*air_pressure)
 
-    end function enhancement_factor_over_ice
+  end function enhancement_factor_over_ice
 
 end module mo_dictionary
 
