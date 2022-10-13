da618cb4f8228b3416c36948c0898198400545f9
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-10-14 00:31:00 +0200

    more compilation flags added

--- compile.sh
+++ compile.sh
@@ -10,7 +10,23 @@ fi
 
 cd build
 
-cmake ..
+d_value=False
+f_value=False
+while getopts "df" opt; do
+  case $opt in
+    d)
+      d_value=True
+      ;;
+    f)
+      f_value=True
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG. Compiling anyway."
+      ;;
+  esac
+done
+
+cmake -DBOUNDS_CHECKS=$d_value -DFAST=$f_value ..
 make
 
 cd ..
