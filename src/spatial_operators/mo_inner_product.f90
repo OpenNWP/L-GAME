f9af469d0a9e47d6ea1d27e80f4d6d5dee359e59
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-10-18 21:08:22 +0200

    simplification

--- src/spatial_operators/mo_inner_product.f90
+++ src/spatial_operators/mo_inner_product.f90
@@ -17,14 +17,14 @@ module mo_inner_product
     ! This subroutine calculates the inner product of two vector fields.
     
     ! input arguments and output
-    real(wp),     intent(in)    :: u_vector_1(:,:,:)    ! vectorfield 1 in x-direction
-    real(wp),     intent(in)    :: v_vector_1(:,:,:)    ! vectorfield 1 in y-direction
-    real(wp),     intent(in)    :: w_vector_1(:,:,:)    ! vectorfield 1 in z-direction
-    real(wp),     intent(in)    :: u_vector_2(:,:,:)    ! vectorfield 2 in x-direction
-    real(wp),     intent(in)    :: v_vector_2(:,:,:)    ! vectorfield 2 in y-direction
-    real(wp),     intent(in)    :: w_vector_2(:,:,:)    ! vectorfield 2 in z-direction
-    real(wp),     intent(inout) :: output_scalar(:,:,:) ! result
-    type(t_grid), intent(in)    :: grid                 ! grid properties
+    real(wp),     intent(in)  :: u_vector_1(:,:,:)    ! vectorfield 1 in x-direction
+    real(wp),     intent(in)  :: v_vector_1(:,:,:)    ! vectorfield 1 in y-direction
+    real(wp),     intent(in)  :: w_vector_1(:,:,:)    ! vectorfield 1 in z-direction
+    real(wp),     intent(in)  :: u_vector_2(:,:,:)    ! vectorfield 2 in x-direction
+    real(wp),     intent(in)  :: v_vector_2(:,:,:)    ! vectorfield 2 in y-direction
+    real(wp),     intent(in)  :: w_vector_2(:,:,:)    ! vectorfield 2 in z-direction
+    real(wp),     intent(out) :: output_scalar(:,:,:) ! result
+    type(t_grid), intent(in)  :: grid                 ! grid properties
     
     ! local variables
     integer :: ji,jk,jl ! spatial indices
