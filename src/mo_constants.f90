0a8c0b7b2d1761d2f5e98111e10a53713f839707
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-10-26 00:10:51 +0200

    formal change

--- src/mo_constants.f90
+++ src/mo_constants.f90
@@ -34,8 +34,8 @@ module mo_constants
   real(wp), parameter :: EPSILON_SECURITY = 1e-10_wp      ! security constant
   real(wp), parameter :: impl_thermo_weight = c_d_v/c_d_p ! time stepping parameter
   
-  ! some properties of the standard atmosphere
-  ! ------------------------------------------
+  ! some properties of the ICAO standard atmosphere
+  ! -----------------------------------------------
   real(wp), parameter :: lapse_rate = 0.0065_wp    ! lapse_rate within the troposphere
   real(wp), parameter :: surface_temp = 288.15_wp  ! the temperature at the surface
   real(wp), parameter :: tropo_height = 11000._wp  ! the tropopause height
