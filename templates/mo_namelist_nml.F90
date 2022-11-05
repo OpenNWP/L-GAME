747b3ad38b8f736afced82197aec90274372f192
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2022-11-05 17:10:09 +0100

    formal changes

---
+++ templates/mo_namelist_nml.F90
@@ -0,0 +1,48 @@
+! This source file is part of the Limited-area GAME version (L-GAME), which is released under the MIT license.
+! Github repository: https://github.com/OpenNWP/L-GAME
+
+module run_nml
+
+  ! This is the namelist that configures the basic run properties of a model integration.
+  
+  use definitions, only: wp
+  
+  implicit none
+  
+  character(len=64) :: run_id ! ID of this run
+  real(wp)          :: dtime  ! time step of this run
+  
+  namelist /run/run_id
+
+  contains
+
+  subroutine run_nml_setup()
+  
+    ! local variables
+    integer :: fileunit
+    
+    ! default values
+    run_id = "ideal"
+    dtime = 5._wp
+    
+    ! open and read namelist file
+    open(action="read",file="namelist.nml",newunit=fileunit)
+    read(nml=run,unit=fileunit)
+        
+    close(fileunit)
+  
+  end subroutine run_nml_setup
+  
+end module run_nml
+
+
+
+
+
+
+
+
+
+
+
+
