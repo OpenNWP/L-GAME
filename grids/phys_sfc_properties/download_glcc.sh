6fea1663cf67f5f914f692701afad4ad4d9b82fb
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2023-01-10 23:01:20 +0100

    formal changes

--- grids/phys_sfc_properties/download_glcc.sh
+++ grids/phys_sfc_properties/download_glcc.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This source file is part of the Limited-area GAME version (L-GAME), which is released under the MIT license.
+# Github repository: https://github.com/OpenNWP/L-GAME
+
 # downloading GLCC data
 
 wget "https://ral.ucar.edu/sites/default/files/public/product-tool/noah-multiparameterization-land-surface-model-noah-mp-lsm/usgs/sfc-fields-usgs-veg30susgs.gz"
--- grids/phys_sfc_properties/download_gldbv2.sh
+++ grids/phys_sfc_properties/download_gldbv2.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This source file is part of the Limited-area GAME version (L-GAME), which is released under the MIT license.
+# Github repository: https://github.com/OpenNWP/L-GAME
+
 # downloading GLDB data
 
 wget "http://www.flake.igb-berlin.de/data/gldbv2.tar.gz"
