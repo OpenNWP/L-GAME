b2351167582c8ed849784ad1b36869eaeeb052ce
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2023-01-11 00:58:42 +0100

    formal changes

--- run_scripts/.sh/download_phys_sfc_quantities.sh
+++ run_scripts/.sh/download_phys_sfc_quantities.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # This source file is part of the Limited-area GAME version (L-GAME), which is released under the MIT license.
 # Github repository: https://github.com/OpenNWP/L-GAME
 
