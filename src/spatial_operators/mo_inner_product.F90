78ffda5baef6db1a9f0186dcee5f4a05f1bb2b68
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2023-01-29 23:27:57 +0100

    formal changes

--- src/spatial_operators/mo_inner_product.F90
+++ src/spatial_operators/mo_inner_product.F90
@@ -2,9 +2,9 @@
 ! Github repository: https://github.com/OpenNWP/L-GAME
 
 module mo_inner_product
-
+  
   ! The calculation of the inner product is executed in this module.
-
+  
   use mo_run_nml,              only: ny,nx,n_layers,n_levels,dy,dx
   use mo_definitions,          only: t_grid,t_state,wp
   use mo_bc_nml,               only: lperiodic
@@ -12,7 +12,7 @@ module mo_inner_product
   implicit none
   
   contains
-
+  
   subroutine inner_product(u_vector_1,v_vector_1,w_vector_1,u_vector_2,v_vector_2,w_vector_2,output_scalar,grid)
     
     ! This subroutine calculates the inner product of two vector fields.
@@ -73,8 +73,13 @@ module mo_inner_product
     !$omp end parallel do
     
   end subroutine w_free_slip
-
+  
 end module mo_inner_product
+
+
+
+
+
 
 
 
