1555eb42db557068b04ede8f3b8e338b30a28341
Max Henrik Balsmeier <mhbalsmeier@gmail.com>
2023-03-06 18:36:02 +0100

    diffusion namelist extended

--- src/namelists/mo_diff_nml.F90
+++ src/namelists/mo_diff_nml.F90
@@ -9,20 +9,22 @@ module mo_diff_nml
   
   implicit none
   
-  logical  :: lmom_diff_h     ! switch for horizontal momentum diffusion
-  logical  :: lmom_diff_v     ! switch for vertical momentum diffusion
-  logical  :: ltemp_diff_h    ! horizontal temperature diffusion switch
-  logical  :: ltemp_diff_v    ! vertical temperature diffusion switch
-  logical  :: lmass_diff_h    ! horizontal mass diffusion switch
-  logical  :: lmass_diff_v    ! vertical mass diffusion switch
-  real(wp) :: h_prandtl       ! height of the Prandtl layer
-  real(wp) :: karman          ! von Karman's constant
-  logical  :: lklemp          ! turns the Klemp damping layer on or off
-  real(wp) :: klemp_damp_max  ! the maximum Klemp damping coefficient
-  real(wp) :: klemp_begin_rel ! lower boundary of the Klemp damping layer in relation to the TOA
+  logical          :: lmom_diff_h         ! switch for horizontal momentum diffusion
+  logical          :: lmom_diff_v         ! switch for vertical momentum diffusion
+  logical          :: ltemp_diff_h        ! horizontal temperature diffusion switch
+  logical          :: ltemp_diff_v        ! vertical temperature diffusion switch
+  logical          :: lmass_diff_h        ! horizontal mass diffusion switch
+  logical          :: lmass_diff_v        ! vertical mass diffusion switch
+  real(wp)         :: h_prandtl           ! height of the Prandtl layer
+  real(wp)         :: karman              ! von Karman's constant
+  logical          :: lklemp              ! turns the Klemp damping layer on or off
+  real(wp)         :: klemp_damp_max      ! the maximum Klemp damping coefficient
+  real(wp)         :: klemp_begin_rel     ! lower boundary of the Klemp damping layer in relation to the TOA
+  character(len=8) :: diff_coeff_scheme_h ! scheme for computing the horizontal diffusion coefficient
+  character(len=8) :: diff_coeff_scheme_v ! scheme for computing the vertical diffusion coefficient
   
   namelist /diff/lmom_diff_h,lmom_diff_v,ltemp_diff_h,ltemp_diff_v,lmass_diff_h,lmass_diff_v,h_prandtl,karman, &
-                 lklemp,klemp_damp_max,klemp_begin_rel
+                 lklemp,klemp_damp_max,klemp_begin_rel,diff_coeff_scheme_h,diff_coeff_scheme_v
   
   contains
   
@@ -43,6 +45,8 @@ module mo_diff_nml
     lklemp = .true.
     klemp_damp_max = 0.25_wp
     klemp_begin_rel = 0.53_wp
+    diff_coeff_scheme_h = "smag"
+    diff_coeff_scheme_v = "tke"
     
     ! Open and read namelist file.
     open(action="read",file="namelist.nml",newunit=fileunit)
